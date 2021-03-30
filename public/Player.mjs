439273db360a71ef27e1730d44765ec226076f7d
gregor-gh <gregor@live.co.uk>
2021-03-30 13:10:40 +0100

    Passing unit tests

--- public/Player.mjs
+++ public/Player.mjs
@@ -16,15 +16,38 @@ class Player {
   }
 
   movePlayer(dir, speed) {
-
+    switch (dir) {
+      case "up":
+        this.y += speed;
+        break;
+      case "down":
+        this.y -= speed;
+        break;
+      case "left":
+        this.x -= speed;
+        break;
+      case "right":
+        this.x += speed;
+        break;
+    }
   }
 
   collision(item) {
-
+    if (item.x == this.x && this.y == item.y)
+      return true;
+    else
+      return false;
   }
 
   calculateRank(arr) {
+    const sorted = arr.sort((a, b) => b.score - a.score);
+    let rank;
+    sorted.forEach((el, i) => {
+      if (el.id == this.id)
+        rank = `Rank: ${i + 1}/${sorted.length}`;
+    })
 
+    return rank;
   }
 }
 
